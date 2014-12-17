57580820999ad43c470060d9e102b76146dcad94
Trompuda <someone@mail.com>
2014-12-17 14:03:59 -0500

    Fix indentation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,168 +1,167 @@
 var bio = {
-"name": "Yozie Rojas",
-"role": "Web Developer",
-"pic":  "images/aboutPic.png",
-"contacts": {
-     "location": "Atlanta, GA",
-     "mobile":   "650-555-5555",
-     "email":    "yozaira@exampte.com",
-     "website":  "http://yoziesite.com"  
-} ,
-"socialLinks": {
-     "github":      "http://github.com" ,
-     "twitter":     "http://twitter.com",
-     "google-plus": "http://googleplus.com"  
- } ,
-"aboutMeLeadPara": "I am Yozie Rojas, do pancetta swine hamburger incididunt in excepteur irure pig labore est venison dolor ex adipisicing. Labore capicola veniam.",
-"aboutMeNextPara":  "<p>Bacon ipsum dolor sit amet shoulder drumstick spare ribs shank, magna in sirloin. Turducken dolore tempor irure tenderloin pork belly shankle"
-                     + "Excepteur in strip steak pork chop voluptate tongue, hamburger nostrud kevin enim nulla ut cow incididunt. commodo corned beef ut non rump swine pork chop exercitation ball tip ham deserunt<\/p>"
+  "name": "Yozie Rojas",
+  "role": "Web Developer",
+  "pic":  "images/aboutPic.png",
+  "contacts": {
+       "location": "Atlanta, GA",
+       "mobile":   "650-555-5555",
+       "email":    "yozaira@exampte.com",
+       "website":  "http://yoziesite.com"  
+  } ,
+  "socialLinks": {
+       "github":      "http://github.com" ,
+       "twitter":     "http://twitter.com",
+       "google-plus": "http://googleplus.com"  
+   } ,
+  "aboutMeLeadPara": "I am Yozie Rojas, do pancetta swine hamburger incididunt in excepteur irure pig labore est venison dolor ex adipisicing. Labore capicola veniam.",
+  "aboutMeNextPara":  "<p>Bacon ipsum dolor sit amet shoulder drumstick spare ribs shank, magna in sirloin. Turducken dolore tempor irure tenderloin pork belly shankle"
+                      +"Excepteur in strip steak pork chop voluptate tongue, hamburger nostrud kevin enim nulla ut cow incididunt. commodo corned beef ut non rump swine pork chop exercitation ball tip ham deserunt<\/p>"
 };
+
 
 var services = {
  "service" : [ 
-  {
-  "title": "Backend Development",
-  "description":  "torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc" 
-                 + "torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }, {
-  "title": "Web Design",
-  "description": "torem ipsun dolor sit '<a href='#'>dolor sit amet<\/a>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc" 
-                 +"torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }, {
-  "title": "Frontend Development",
-  "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }, {
-  "title": "Digital Marketing",
-  "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }
-]
+    {
+    "title": "Backend Development",
+    "description": "torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc" 
+                  + "torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }, {
+    "title": "Web Design",
+    "description": "torem ipsun dolor sit '<a href='#'>dolor sit amet<\/a>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc" 
+                   +"torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }, {
+    "title": "Frontend Development",
+    "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }, {
+    "title": "Digital Marketing",
+    "description": "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }
+  ]
 };
 
 
 
 var skills = {
- "skill" : [ 
-  {
-  "title": "PHP Programming",
-  "proficiencyLevel": "70",
-  "description": "torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc" 
-  }, {
-  "title": "MySQL Database",
-  "proficiencyLevel": "70",
-  "description": "torem ipsun dolor sit '<a href='#'>dolor sit amet<\/a>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"      
-  }, {      
-  "title": "HTML-CSS",
-  "proficiencyLevel": "80",
-  "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }, {
-  "title": "JavaScript",
-  "proficiencyLevel": "60",
-  "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }, {
-  "title": "Git Version Control",
-  "proficiencyLevel": "60",
-  "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }, {
-  "title": "Google Map API",
-  "proficiencyLevel": "60",
-  "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
-  }
-]
+ "skill": [ 
+    {
+    "title": "PHP Programming",
+    "proficiencyLevel": "70",
+    "description": "torem ipsun dolor sit '<b>dolor sit amet<\/b>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc" 
+    }, {
+    "title": "MySQL Database",
+    "proficiencyLevel": "70",
+    "description": "torem ipsun dolor sit '<a href='#'>dolor sit amet<\/a>', etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"      
+    }, {      
+    "title": "HTML-CSS",
+    "proficiencyLevel": "80",
+    "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }, {
+    "title": "JavaScript",
+    "proficiencyLevel": "60",
+    "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }, {
+    "title": "Git Version Control",
+    "proficiencyLevel": "60",
+    "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }, {
+    "title": "Google Map API",
+    "proficiencyLevel": "60",
+    "description":  "torem ipsun dolor sit anet etc etc etc.torem ipsun dolor sit anet etc etc etc torem ipsun dolor sit anet etc etc etc"
+    }
+  ]
 };
 
 
 var projects = {
   "project": [
     {
-      "title": "MongoDB CMS",
+    "title": "MongoDB CMS",
+    "date": "2013",
+    "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore ",
+    "githubLink": "http://github.com",
+    "images": [
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300"
+      ] 
+    }, {
+      "title": "PHP-HTML Photo Gallery",
       "date": "2013",
-      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore ",
+      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "githubLink": "http://github.com",
+      "images": [
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300"
+      ] 
+    }, {
+      "title": "SMS Phone App",
+      "date": "2013",
+      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "githubLink": "http://github.com",
+      "images": [
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300"
+      ] 
+    }, {
+      "title": "Responsive Professional Theme",
+      "date": "2013",
+      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "githubLink": "http://github.com",
+      "images": [
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300",
+        "http://placehold.it/555x300"
+      ] 
+    }, {
+      "title": "Google Map Business App",
+      "date": "2013",
+      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       "githubLink": "http://github.com",
       "images": [
           "http://placehold.it/555x300",
           "http://placehold.it/555x300",
           "http://placehold.it/555x300"
-        ] 
-      }, {
-          "title": "PHP-HTML Photo Gallery",
-          "date": "2013",
-          "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "githubLink": "http://github.com",
-          "images": [
-            "http://placehold.it/555x300",
-            "http://placehold.it/555x300",
-            "http://placehold.it/555x300"
-          ] 
-      }, {
-          "title": "SMS Phone App",
-          "date": "2013",
-          "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "githubLink": "http://github.com",
-          "images": [
-            "http://placehold.it/555x300",
-            "http://placehold.it/555x300",
-            "http://placehold.it/555x300"
-          ] 
-      }, {
-          "title": "Responsive Professional Theme",
-          "date": "2013",
-          "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "githubLink": "http://github.com",
-          "images": [
-            "http://placehold.it/555x300",
-            "http://placehold.it/555x300",
-            "http://placehold.it/555x300"
-          ] 
-      }, {
-          "title": "Google Map Business App",
-          "date": "2013",
-          "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "githubLink": "http://github.com",
-          "images": [
-              "http://placehold.it/555x300",
-              "http://placehold.it/555x300",
-              "http://placehold.it/555x300"
            ] 
-      }, {
-          "title": "Google Map Business App",
-          "date": "2013",
-          "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "githubLink": "http://github.com",
-          "images": [
-              "http://placehold.it/555x300",
-              "http://placehold.it/555x300",
-              "http://placehold.it/555x300"
-           ] 
-      }
+    }, {
+      "title": "Google Map Business App",
+      "date": "2013",
+      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore datat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "githubLink": "http://github.com",
+      "images": [
+          "http://placehold.it/555x300",
+          "http://placehold.it/555x300",
+          "http://placehold.it/555x300"
+       ] 
+    }
   ]
 };
 
 
 var work = {
-
   "jobs": [
-     {
+    {
       "employer": "Freelance",
       "title": "Web Developer",
       "dates": "Jan 2012- Present",
       "city": "Washington, DC",
       "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
-      }, {
-        "employer": "USAID Child Health Integrated Program (MCHIP)",
-        "title": "Communication & Social Media Assistant",
-        "dates": "Jan 2010- November 2011",
-        "city": "Atlanta, GA",
-        "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
-      }, {
-        "employer": "Administrative Assistant",
-        "title": "Communication and PR Assistant",
-        "dates": "Feb 2008 - Dic 2009",
-        "city": "Miami, Fl",
-        "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
-      }
+    }, {
+      "employer": "USAID Child Health Integrated Program (MCHIP)",
+      "title": "Communication & Social Media Assistant",
+      "dates": "Jan 2010- November 2011",
+      "city": "Atlanta, GA",
+      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
+    }, {
+      "employer": "Administrative Assistant",
+      "title": "Communication and PR Assistant",
+      "dates": "Feb 2008 - Dic 2009",
+      "city": "Miami, Fl",
+      "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
+    }
   ]
 };
-
 
 
 /*
@@ -170,15 +169,15 @@ Method to display web development services
 */
 services.display = function (services_obj) {  
 
-  //  console.log(Object.keys(this.service));
+  // console.log(Object.keys(this.service));
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 
   if(this.service.length > 0) { 
     for (var key in services_obj.service) { 
       // console.log (key) ;
       // console.log (this.service[key][0]) ;
-      //console.log (this.service[key]) ;
-      //console.log (this.service.indexOf(this.service[key]) );
+      // console.log (this.service[key]) ;
+      // console.log (this.service.indexOf(this.service[key]) );
       // http://msdn.microsoft.com/en-us/library/ie/ff679977%28v=vs.94%29.aspx
 
       // div that will containt service info
@@ -201,7 +200,6 @@ services.display = function (services_obj) {
       serviceItemStart.appendChild(serviceItem);
 
       // add a specific color and icon to each title
-
       if (this.service.indexOf(this.service[key]) === 0 ) {
 
         serviceItemDiv.className = 'bg-peach';
@@ -255,7 +253,6 @@ services.display = function (services_obj) {
 
 
 
-
 /*
 Method to display personal information
 */
@@ -263,7 +260,7 @@ bio.displayInfo = function (bio_object) {
 
      if(bio_object) {     
     // test object
-    // return(console.log (this),  console.log (this.name)  )
+    // return(console.log (this),  console.log (this.name))
     
     // select container to append elements  
     var bioStart = document.querySelector('#about .row'); 
@@ -306,7 +303,6 @@ bio.displayInfo = function (bio_object) {
 }
 
 
-
 /*
 Method to create and display a progress bar showing the skill levels
 */
@@ -326,7 +322,8 @@ skills.createProgressBar = function (divToAppend, percent, bgColor) {
   progressBar.setAttribute('aria-valuenow', percent);
   progressBar.setAttribute('aria-valuemin', 0);
   progressBar.setAttribute('aria-valuemax', 100);
-  // progressBar.style.width  = percent +'%'; // this will create a static width. Disable and add the ease-out class
+  // this will create a static width. Disable and add the ease-out class
+  // progressBar.style.width  = percent +'%'; 
   progressBar.innerHTML = '<p>' + percent +'%' +'</p>';
     
   // append elements
@@ -334,12 +331,13 @@ skills.createProgressBar = function (divToAppend, percent, bgColor) {
   progressContainer.appendChild(progressBar);     
 
   // jQuery provides document.ready, which abstracts across browsers quirks away, 
-  //and fires as soon as the page's DOM is ready (doesn't wait for images etc.).
-  // window.onload vs $(document).ready()  http://stackoverflow.com/questions/3698200/window-onload-vs-document-ready
+  // and fires as soon as the page's DOM is ready (doesn't wait for images etc.).
+  // window.onload vs $(document).ready() http://stackoverflow.com/questions/3698200/window-onload-vs-document-ready
   // native event  http://caniuse.com/#feat=domcontentloaded
-  if ( document.addEventListener ) {   
-    document.addEventListener("DOMContentLoaded", function(){  // this is working on FFox and Chrome, and IE11
-     skills.animateBar();  // call animateBar method on document ready, or animation wont work  
+  if ( document.addEventListener ) { 
+      // this is working on FFox and Chrome, and IE11  
+      document.addEventListener("DOMContentLoaded", function(){  
+      skills.animateBar(); // call animateBar method on document ready, or animation wont work  
       }, false);
   }
 } 
@@ -348,7 +346,7 @@ skills.createProgressBar = function (divToAppend, percent, bgColor) {
 
 /*
 Method to add a percentage value width according to the value of aria-valuenow.
-// Call it on createProgressBar() method to animate the bar
+Call it on createProgressBar() method to animate the bar
 */
 skills.animateBar = function (){
   var bars = document.querySelectorAll('div.progress-bar') ;  
@@ -360,8 +358,7 @@ skills.animateBar = function (){
                  // this aill add the with attribute value to each bar, but will not be animated
 
  
-  
-  
+   
 /*
 Method to create and display skills
 */  
@@ -444,14 +441,14 @@ skills.display = function (object) {
     }
   }
 }
-skills.display(skills); // Why this method dont work when called on index.html, like the rest???
+skills.display(skills); // Why this method dont work when called on index.html -- ???
 
 
 
 /*
 Method to display projects
 */
-  projects.display = function (projects_obj) {  
+projects.display = function (projects_obj) {  
 
   if(projects_obj.project.length > 0) { 
       for (var key in projects_obj.project) { 
@@ -502,7 +499,6 @@ Method to display projects
 Method to display social links
 */
 bio.displaySocialLinks = function(bio_object) {
-
   // var link = document.querySelector('#main-footer a') ;
   // link.href = 'www.mynewsite.com'; 
   // console.log(link);
@@ -520,12 +516,10 @@ bio.displaySocialLinks = function(bio_object) {
 
 
 
-
 /*
 Method to display Copy Rights Info
 */
 bio.displayCopyRights = function (bio_object) {
-
   var date = new Date();
   var year = date.getFullYear();
   var copyRightContainer = document.querySelector ('#main-footer #copy-rights');

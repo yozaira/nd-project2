2a532bc6b723c7573813664a047403c9f11e0d17
Trompuda <someone@mail.com>
2014-12-16 21:02:07 -0500

    Create googleMap.js and add the json object that contains the work data to be displayed on the map

---
+++ js/googleMap.js
@@ -0,0 +1,24 @@
+var work = {
+
+    "jobs": [
+	     {
+            "employer": "Freelance",
+            "title": "Web Developer",
+            "dates": "Jan 2012- Present",
+            "city": "Washington, DC",
+            "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
+        } ,  {
+            "employer": "USAID Child Health Integrated Program (MCHIP)",
+            "title": "Communication & Social Media Assistant",
+            "dates": "Jan 2010- November 2011",
+			 "city": "Atlanta, GA",
+            "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
+        } ,  {
+            "employer": "Administrative Assistant",
+            "title": "Communication and PR Assistant",
+            "dates": "Feb 2008 - Dic 2009",
+			 "city": "Miami, Fl",
+            "description": "Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ."
+        }
+    ]
+};
